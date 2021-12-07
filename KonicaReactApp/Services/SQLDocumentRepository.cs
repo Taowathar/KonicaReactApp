e616ac34da689060248b2f349aac5723acf92c35
Taowathar <taowathar@gmail.com>
2021-12-07 19:44:16 +0100

    Create SQLDocumentRepository class

---
+++ KonicaReactApp/Services/SQLDocumentRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using KonicaReactApp.Data;
+using KonicaReactApp.Models;
+
+namespace KonicaReactApp.Services
+{
+    public class SQLDocumentRepository : IDocumentRepository
+    {
+        private readonly konicaContext _context;
+
+        public SQLDocumentRepository(konicaContext context)
+        {
+            this._context = context;
+        }
+        public IEnumerable<Document> GetDocumentsByMainId(int id)
+        {
+            return _context.Dokumentumok.Where(doc => doc.MainId == id);
+        }
+    }
+}
