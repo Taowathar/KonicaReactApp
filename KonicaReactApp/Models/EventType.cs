daa80f5821afc7bf8b6dabe48d855e7cf0e79658
Taowathar <taowathar@gmail.com>
2021-12-07 13:51:30 +0100

    Generate DbContext and model classes with Entity Framework's Scaffold-DBContext command

---
+++ KonicaReactApp/Models/EventType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace KonicaReactApp.Models
+{
+    public partial class EventType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+
+    }
+}
