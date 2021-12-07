837e5c31f92e81dc60c8c448e59032ba5a4fa352
Taowathar <taowathar@gmail.com>
2021-12-07 13:04:03 +0100

    Create signin page

---
+++ KonicaReactApp/ClientApp/src/pages/signin.js
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import SignIn from '../components/SignIn'
+
+const SignInPage = () => {
+    return (
+        <>
+            <SignIn />
+        </>
+    );
+}
+
+export default SignInPage;
