1febda8660cc93001f9bd3e085b95a23bcf40349
Taowathar <taowathar@gmail.com>
2021-12-07 12:25:58 +0100

    Create Navbar component

---
+++ KonicaReactApp/ClientApp/src/components/Navbar/index.js
@@ -0,0 +1,24 @@
+﻿import React from 'react';
+import { Nav, NavbarContainer, NavLogo, NavMenu, NavLinks, NavBtn, NavBtnLink } from './NavbarElements';
+
+
+
+const Navbar = () => {
+
+    return (
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>KonicaMinolta</NavLogo>
+                <NavMenu>
+                    <NavLinks to="/documents">Documents</NavLinks>
+                </NavMenu>
+                <NavBtn>
+                    <NavBtnLink to="/signup">Sign Up</NavBtnLink>
+                    <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                </NavBtn>
+            </NavbarContainer>
+        </Nav>
+    );
+}
+
+export default Navbar;
