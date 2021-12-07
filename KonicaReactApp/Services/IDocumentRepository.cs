b3385effdc90766efa01276c77418a861d73d5eb
Taowathar <taowathar@gmail.com>
2021-12-07 19:43:26 +0100

    Create IDocumentRepository.cs interface

---
+++ KonicaReactApp/Services/IDocumentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KonicaReactApp.Models;
+
+namespace KonicaReactApp.Services
+{
+    public interface IDocumentRepository
+    {
+        public IEnumerable<Document> GetDocumentsByMainId(int id);
+    }
+}
