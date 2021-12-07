dcb94aedf853e623c776aefa82c747833e0a67f5
Taowathar <taowathar@gmail.com>
2021-12-07 19:41:29 +0100

    Create Document element

---
+++ KonicaReactApp/ClientApp/src/components/Document/index.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import { TableRow, TableData } from './DocumentElements';
+
+const Document = ({ document }) => {
+    return (
+        <TableRow>
+            <TableData>{document.id}</TableData>
+            <TableData>{document.title}</TableData>
+            <TableData>{document.extension}</TableData>
+            <TableData>{document.mainId}</TableData>
+            <TableData>{document.source}</TableData>
+        </TableRow>
+    );
+}
+
+export default Document;
