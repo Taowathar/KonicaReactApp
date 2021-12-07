e4f11954e673db390ff58da51bf2c0a236221979
Taowathar <taowathar@gmail.com>
2021-12-07 17:32:13 +0100

    Create table header in DocumentList component

--- KonicaReactApp/ClientApp/src/components/DocumentList/DocumentListElements.js
+++ KonicaReactApp/ClientApp/src/components/DocumentList/DocumentListElements.js
@@ -1,1 +1,29 @@
 ﻿import styled from 'styled-components';
+
+
+export const TableContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const DocumentsTable = styled.table`
+    margin-top: 50px;
+    width: 90%;
+    border-radius: 15px;
+`;
+
+export const TableHead = styled.thead`
+    font-weight: 400;
+    background: #000;
+    color: #fff;
+`;
+
+export const TableHeader = styled.tr`
+`;
+
+export const HeaderCell = styled.th`
+
+`;
--- KonicaReactApp/ClientApp/src/components/DocumentList/index.js
+++ KonicaReactApp/ClientApp/src/components/DocumentList/index.js
@@ -1,9 +1,21 @@
 ﻿import React from 'react';
+import { TableContainer, DocumentsTable, TableHead, TableHeader, HeaderCell } from './DocumentListElements'
 
 const DocumentList = () => {
     return (
-        <>
-        </>
+        <TableContainer>
+            <DocumentsTable>
+                <TableHead>
+                    <TableHeader>
+                        <HeaderCell>Id</HeaderCell>
+                        <HeaderCell>Title</HeaderCell>
+                        <HeaderCell>Extension</HeaderCell>
+                        <HeaderCell>MainId</HeaderCell>
+                        <HeaderCell>Source</HeaderCell>
+                    </TableHeader>
+                </TableHead>
+            </DocumentsTable>
+        </TableContainer>
     );
 }
 
