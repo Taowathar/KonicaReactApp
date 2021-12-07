13c4e81901ac53878cfebf7693b420cea2031f07
Taowathar <taowathar@gmail.com>
2021-12-07 14:21:20 +0100

    Create SQLUserRepository class

---
+++ KonicaReactApp/Services/SQLUserRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KonicaReactApp.Data;
+using KonicaReactApp.Models;
+
+namespace KonicaReactApp.Services
+{
+    public class SQLUserRepository : IUserRepository
+    {
+
+        private readonly konicaContext _context;
+
+        public SQLUserRepository(konicaContext context)
+        {
+            this._context = context;
+        }
+
+        public User GetUserByName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
