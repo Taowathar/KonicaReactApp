8d6122ae3a11f2099029084a08396098b77e3497
Taowathar <taowathar@gmail.com>
2021-12-07 14:47:02 +0100

    Change property types in User class

--- KonicaReactApp/Models/User.cs
+++ KonicaReactApp/Models/User.cs
@@ -7,9 +7,9 @@ namespace KonicaReactApp.Models
 {
     public partial class User
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public string Salt { get; set; }
+        public byte[] Salt { get; set; }
     }
 }
