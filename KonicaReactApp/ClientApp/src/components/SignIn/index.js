7975cfe1f39e8a6b4ba43202c845e76521bb482b
Taowathar <taowathar@gmail.com>
2021-12-07 13:02:49 +0100

    Create SignIn component

---
+++ KonicaReactApp/ClientApp/src/components/SignIn/index.js
@@ -0,0 +1,28 @@
+﻿import React from 'react';
+
+import { Container, FormWrap, Icon, FormContent, Form, FormH1, FormLabel, FormInput, FormButton} from './SignInElements'
+
+const SignIn = () => {
+
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to="/">KonicaMinolta</Icon>
+                    <FormContent>
+                        <Form>
+                            <FormH1>Sign in to your account</FormH1>
+                            <FormLabel htmlFor='for'>User Name</FormLabel>
+                            <FormInput type='text' placeholder='User Name' required />
+                            <FormLabel htmlFor='for'>Password</FormLabel>
+                            <FormInput type='password' placeholder='Password' required/>
+                            <FormButton type='submit'>Continue</FormButton>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>
+    );
+}
+
+export default SignIn;
