8e0827e7a83b6042b2f0e88888fc5f002cae5431
Taowathar <taowathar@gmail.com>
2021-12-07 14:32:41 +0100

    Create UserController class

---
+++ KonicaReactApp/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KonicaReactApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KonicaReactApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IUserRepository SqlUserRepository { get; set; }
+
+        public UserController(IUserRepository userRepository)
+        {
+            this.SqlUserRepository = userRepository;
+        }
+    }
+}
