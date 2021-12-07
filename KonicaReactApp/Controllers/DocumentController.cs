f5ebd7f766f8ce6461b6f7685c653294bdd9f63d
Taowathar <taowathar@gmail.com>
2021-12-07 19:45:32 +0100

    Create DocumentController class

---
+++ KonicaReactApp/Controllers/DocumentController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using KonicaReactApp.Models;
+using KonicaReactApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KonicaReactApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentController : ControllerBase
+    {
+        public IDocumentRepository SqlDocumentRepository { get; set; }
+
+        public DocumentController(IDocumentRepository documentRepository)
+        {
+            this.SqlDocumentRepository = documentRepository;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Document> GetDocumentsByMainId(int id)
+        {
+            return SqlDocumentRepository.GetDocumentsByMainId(id);
+        }
+    }
+}
