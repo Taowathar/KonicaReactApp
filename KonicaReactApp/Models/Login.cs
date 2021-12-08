5c4155a194727a85c54f4d8cd8303b7bc436fbf8
Taowathar <taowathar@gmail.com>
2021-12-08 23:08:12 +0100

    Change Login class Id property type to string

--- KonicaReactApp/Models/Login.cs
+++ KonicaReactApp/Models/Login.cs
@@ -2,13 +2,13 @@
 
 namespace KonicaReactApp.Models
 {
-    public partial class Login
+    public class Login
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string ClientIpAddress { get; set; }
         public DateTime LoginTime { get; set; }
         public string LoginName { get; set; }
-        public int? EventId { get; set; }
+        public int EventId { get; set; }
 
         public virtual EventType Event { get; set; }
     }
