d51b13bcb452904083233a2e35aedd77ff647b79
Taowathar <taowathar@gmail.com>
2021-12-08 14:10:04 +0100

    Create SQLNaploRepository class

---
+++ KonicaReactApp/Services/SQLNaploRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KonicaReactApp.Data;
+using KonicaReactApp.Models;
+
+namespace KonicaReactApp.Services
+{
+    public class SQLNaploRepository : INaploRepository
+    {
+        private readonly konicaContext _context;
+
+        public SQLNaploRepository(konicaContext context)
+        {
+            this._context = context;
+        }
+
+        public IQueryable<dynamic> GetAllEventsByDocumentId(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
