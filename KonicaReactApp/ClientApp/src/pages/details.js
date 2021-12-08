0c10ecd8b5fa5c621acb663761630df3cee2fdac
Taowathar <taowathar@gmail.com>
2021-12-08 11:29:09 +0100

    Create details page

---
+++ KonicaReactApp/ClientApp/src/pages/details.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import Navbar from '../components/Navbar'
+import DocumentDetails from '../components/DocumentDetails'
+
+const DetailsPage = ({ user, loggedIn, documentId }) => {
+    return (
+        <>
+            <Navbar user={user} loggedIn={loggedIn} />
+            <DocumentDetails documentId={documentId} />
+        </>
+    );
+}
+
+export default DetailsPage;
