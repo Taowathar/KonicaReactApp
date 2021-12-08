3173a9ba4c26ca4d7e3c9e790eb9dc89621c76f1
Taowathar <taowathar@gmail.com>
2021-12-08 22:48:44 +0100

    Create LoginController class

---
+++ KonicaReactApp/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KonicaReactApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KonicaReactApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public ILoginRepository SqlLoginRepository { get; set; }
+
+        public LoginController(ILoginRepository loginRepository)
+        {
+            this.SqlLoginRepository = loginRepository;
+        }
+    }
+}
