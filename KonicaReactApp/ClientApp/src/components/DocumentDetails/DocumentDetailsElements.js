0dd06bf3084f192ad26c7d9c0ee54e13926dff46
Taowathar <taowathar@gmail.com>
2021-12-08 12:13:32 +0100

    Add tables to DocumentDetails component

--- KonicaReactApp/ClientApp/src/components/DocumentDetails/DocumentDetailsElements.js
+++ KonicaReactApp/ClientApp/src/components/DocumentDetails/DocumentDetailsElements.js
@@ -1,1 +1,60 @@
 ﻿import styled from 'styled-components';
+
+export const DetailsH1 = styled.h1`
+    margin-top: 30px;
+    text-align: center;
+    font-size: 40px;
+    font-weight: 700;
+`;
+
+export const TableContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+export const EventTable = styled.table`
+    margin-top: 50px;
+    width: 40%;
+    font-size: 20px;
+`;
+
+export const EventTableHead = styled.thead`
+    background: burlywood;
+    text-align: center;
+`;
+export const EventTableHeader = styled.tr`
+    height: 30px;
+`;
+
+export const EventHeaderCell = styled.th`
+    border: 1px solid #01bf71;
+`;
+
+export const EventTableBody = styled.tbody`
+
+`;
+
+export const ChildTable = styled.table`
+    margin-top: 50px;
+    width: 40%;
+    font-size: 20px;
+`;
+
+export const ChildTableHead = styled.thead`
+    background: brown;
+    text-align: center;
+`;
+export const ChildTableHeader = styled.tr`
+        height: 30px;
+`;
+
+export const ChildHeaderCell = styled.th`
+    border: 1px solid #01bf71;
+`;
+
+export const ChildTableBody = styled.tbody`
+
+`;
--- KonicaReactApp/ClientApp/src/components/DocumentDetails/index.js
+++ KonicaReactApp/ClientApp/src/components/DocumentDetails/index.js
@@ -1,9 +1,36 @@
 ﻿import React from 'react';
+import { DetailsH1,TableContainer, EventTable, EventTableHead, EventTableHeader, EventHeaderCell, EventTableBody, ChildTable, ChildTableHead, ChildTableHeader, ChildHeaderCell, ChildTableBody} from './DocumentDetailsElements'
 
 const DocumentDetails = ({ documentId }) => {
     return (
         <>
-            <h1>{documentId}</h1>
+        <DetailsH1>TITLE</DetailsH1>
+        <TableContainer>
+            <EventTable>
+                <EventTableHead>
+                    <EventTableHeader>
+                        <EventHeaderCell>Event</EventHeaderCell>
+                        <EventHeaderCell>Time</EventHeaderCell>
+                    </EventTableHeader>
+                </EventTableHead>
+                <EventTableBody>
+                    
+                </EventTableBody>
+            </EventTable>
+
+            <ChildTable>
+                <ChildTableHead>
+                    <ChildTableHeader>
+                        <ChildHeaderCell>Title</ChildHeaderCell>
+                        <ChildHeaderCell>Source</ChildHeaderCell>
+                        <ChildHeaderCell>Time</ChildHeaderCell>
+                    </ChildTableHeader>
+                </ChildTableHead>
+                <ChildTableBody>
+
+                </ChildTableBody>
+            </ChildTable>
+            </TableContainer>
         </>
     );
 }
