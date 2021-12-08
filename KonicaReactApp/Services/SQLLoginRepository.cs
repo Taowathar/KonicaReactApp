a8ef60dd4797d9c05787ac0cd485240d2b4c6a0c
Taowathar <taowathar@gmail.com>
2021-12-08 22:48:33 +0100

    Create SQLLoginRepository class

---
+++ KonicaReactApp/Services/SQLLoginRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KonicaReactApp.Data;
+using KonicaReactApp.Models;
+
+namespace KonicaReactApp.Services
+{
+    public class SQLLoginRepository : ILoginRepository
+    {
+        private readonly konicaContext _context;
+
+        public SQLLoginRepository(konicaContext context)
+        {
+            this._context = context;
+        }
+
+        public void AddLogin(Login login)
+        {
+            _context.Logins.Add(login);
+            _context.SaveChanges();
+        }
+    }
+}
