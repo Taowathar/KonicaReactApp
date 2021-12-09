94a80ed74e7f52a221931edab65633b7cb90d577
Taowathar <taowathar@gmail.com>
2021-12-09 02:02:39 +0100

    Add GetFailedLogins/GetAccountDisabled methods to ILoginRepository interface

--- KonicaReactApp/Services/ILoginRepository.cs
+++ KonicaReactApp/Services/ILoginRepository.cs
@@ -5,5 +5,7 @@ namespace KonicaReactApp.Services
     public interface ILoginRepository
     {
         void AddLogin(Login login);
+        int GetFailedLogins(string userName);
+        bool GetAccountDisabled(string userName);
     }
 }
